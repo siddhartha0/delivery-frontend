8d330b5e78c80eb26d0f28608890acc424083000
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-12-13 19:02:15 +0545

    folder structure init

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} path="/" />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
