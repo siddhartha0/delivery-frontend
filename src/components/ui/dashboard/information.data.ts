f391c7bbc0f555e3aeec6985e41adc3d3e076e44
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-12-19 18:40:44 +0545

    information panel data fixed

--- src/components/ui/dashboard/information.data.ts
+++ src/components/ui/dashboard/information.data.ts
@@ -1,25 +1,25 @@
 export const informations = [
   {
     id: "1",
-    type: "order",
+    type: "order" as "order",
     message: "New order #1234 has been placed",
     time: "2 minutes ago",
   },
   {
     id: "2",
-    type: "user",
+    type: "user" as "user",
     message: "User John Doe has registered",
     time: "1 hour ago",
   },
   {
     id: "3",
-    type: "alert",
+    type: "alert" as "alert",
     message: "Server load is high",
     time: "3 hours ago",
   },
   {
     id: "4",
-    type: "order",
+    type: "order" as "order",
     message: "Order #9876 has been delivered",
     time: "5 hours ago",
   },
